993d321df8c01d8de10862c7f38757ca0a40276d
Jorpheus7 <jordan.gobley.pro@gmail.com>
2016-01-22 11:16:11 +0100

    Implementing the fragments

--- app/src/main/java/fr/android/androidexercises/Step1Fragment.java
+++ app/src/main/java/fr/android/androidexercises/Step1Fragment.java
@@ -1,9 +1,32 @@
 package fr.android.androidexercises;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class Step1Fragment extends Fragment {
 
-    // TODO Override onCreateViewMethod
+    private TextView textView;
+     // TODO Override onCreateViewMethod
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_step1, container, false);
+        // TODO findViewById textView (TextView)
+        textView = (TextView) view.findViewById(R.id.step1TextView);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // TODO setText(step0)
+        textView.setText("Fragment 2");
+    }
 
 }
