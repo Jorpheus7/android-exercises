4fc120bbab35dd40eb1ef814a7b2e00c6f4c0725
Jorpheus7 <jordan.gobley.pro@gmail.com>
2016-01-22 08:23:54 +0100

    Creating the listview with the adapter

--- app/src/main/java/fr/android/androidexercises/BookItemView.java
+++ app/src/main/java/fr/android/androidexercises/BookItemView.java
@@ -2,9 +2,15 @@ package fr.android.androidexercises;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class BookItemView extends LinearLayout {
+
+    private TextView nameTextView;
+    private TextView priceTextView;
 
     public BookItemView(Context context) {
         this(context, null);
@@ -22,9 +28,21 @@ public class BookItemView extends LinearLayout {
     protected void onFinishInflate() {
         super.onFinishInflate();
         // TODO findViewById()
+        nameTextView = (TextView) findViewById(R.id.nameTextView);
+        priceTextView = (TextView) findViewById(R.id.priceTextView);
+
+        nameTextView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), nameTextView.getText(), Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
     public void bindView(Book book) {
         // TODO setText()
+        nameTextView.setText(book.name);
+        priceTextView.setText(String.valueOf(book.price));
     }
 }
